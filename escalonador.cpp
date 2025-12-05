04031ca3b5ad8054db22b2f21666adb10975c585
ckaincode <caiocesar20032003@gmail.com>
2025-12-05 06:38:50 -0300

    refct: scheduler

---
+++ escalonador.cpp
@@ -0,0 +1,166 @@
+#include <deque>
+#include <vector>
+#include <iostream>
+
+#include "processo.h"
+#include "escalonador.h"
+
+void Escalonador::adicionar(Processo *p)
+{
+    if (p->prioridade_atual == 0)
+    {
+        fila_tr.push_back(p);
+    }
+    else if (p->prioridade_atual == 1)
+    {
+        fila_p1.push_back(p);
+    }
+    else if (p->prioridade_atual == 2)
+    {
+        fila_p2.push_back(p);
+    }
+    else if (p->prioridade_atual == 3)
+    {
+        fila_p3.push_back(p);
+    }
+    else if (p->prioridade_atual == 4)
+    {
+        fila_p4.push_back(p);
+    }
+    else
+    {
+        fila_p5.push_back(p);
+    }
+}
+
+Processo *Escalonador::proximo()
+{
+    if (!fila_tr.empty())
+        return fila_tr.front();
+    if (!fila_p1.empty())
+        return fila_p1.front();
+    if (!fila_p2.empty())
+        return fila_p2.front();
+    if (!fila_p3.empty())
+        return fila_p3.front();
+    if (!fila_p4.empty())
+        return fila_p4.front();
+    if (!fila_p5.empty())
+        return fila_p5.front();
+    return NULL;
+}
+
+void Escalonador::remover_da_frente(Processo *p)
+{
+    int prioridade = p->prioridade_atual;
+    if (prioridade == 0 && !fila_tr.empty())
+        fila_tr.pop_front();
+    else if (prioridade == 1 && !fila_p1.empty())
+        fila_p1.pop_front();
+    else if (prioridade == 2 && !fila_p2.empty())
+        fila_p2.pop_front();
+    else if (prioridade == 3 && !fila_p3.empty())
+        fila_p3.pop_front();
+    else if (prioridade == 4 && !fila_p4.empty())
+        fila_p4.pop_front();
+    else if (prioridade == 5 && !fila_p5.empty())
+        fila_p5.pop_front();
+}
+
+void Escalonador::executar_aging()
+{
+
+    // Verifica Fila 5
+    int tamanho = fila_p5.size();
+    for (int i = 0; i < tamanho; i++)
+    {
+        Processo *p = fila_p5.front(); // Pega o primeiro
+        fila_p5.pop_front();           // Tira da fila
+
+        p->tempo_espera++; // Envelhece o processo (mais um ciclo sem CPU)
+
+        // Se esperou 10 ciclos, sobre a prioridade
+        if (p->tempo_espera >= 10)
+        {
+            p->prioridade_atual--; // Sobe para p4
+            p->tempo_espera = 0;   //
+            // Promove para a fila de cima (No caso p4)
+            fila_p4.push_back(p);
+            std::cout << ">>> [AGING] Processo " << p->PID << " subiu para P4" << std::endl;
+        }
+        else
+        {
+            // Se nao envelheceu o suficiente, volta pro final da fila 5
+            fila_p5.push_back(p);
+        }
+    }
+
+    /*
+    Desse ponto até o final da função 'executar_aging()' o processo é o mesmo que o suado na fila 5
+    Esta na ordem 4,3,2 de verificação
+    NÃO HÁ PROMOÇÃO PARA A FILA TR
+    */
+
+    // Verifica Fila 4
+    tamanho = fila_p4.size();
+    for (int i = 0; i < tamanho; i++)
+    {
+        Processo *p = fila_p4.front();
+        fila_p4.pop_front();
+
+        p->tempo_espera++;
+        if (p->tempo_espera >= 10)
+        {
+            p->prioridade_atual--; // Sobe para 3
+            p->tempo_espera = 0;
+            fila_p3.push_back(p);
+            std::cout << ">>> [AGING] Processo " << p->PID << " subiu para P3" << std::endl;
+        }
+        else
+        {
+            fila_p4.push_back(p);
+        }
+    }
+
+    // Verifica Fila 3
+    tamanho = fila_p3.size();
+    for (int i = 0; i < tamanho; i++)
+    {
+        Processo *p = fila_p3.front();
+        fila_p3.pop_front();
+
+        p->tempo_espera++;
+        if (p->tempo_espera >= 10)
+        {
+            p->prioridade_atual--; // Sobe para 2
+            p->tempo_espera = 0;
+            fila_p2.push_back(p);
+            std::cout << ">>> [AGING] Processo " << p->PID << " subiu para P2" << std::endl;
+        }
+        else
+        {
+            fila_p3.push_back(p);
+        }
+    }
+
+    // Verifica Fila 2
+    tamanho = fila_p2.size();
+    for (int i = 0; i < tamanho; i++)
+    {
+        Processo *p = fila_p2.front();
+        fila_p2.pop_front();
+
+        p->tempo_espera++;
+        if (p->tempo_espera >= 10)
+        {
+            p->prioridade_atual--; // Sobe para 1
+            p->tempo_espera = 0;
+            fila_p1.push_back(p);
+            std::cout << ">>> [AGING] Processo " << p->PID << " subiu para P1" << std::endl;
+        }
+        else
+        {
+            fila_p2.push_back(p);
+        }
+    }
+}
